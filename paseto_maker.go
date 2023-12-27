fcf7630a8e2c4c72ca2899a04b8e7d7955379ff6
anfen93 <anfen93@gmail.com>
2023-12-27 15:50:03 +0100

    improved tests, added comments on paseto_maker.go

--- paseto_maker.go
+++ paseto_maker.go
@@ -1,32 +1,53 @@
+// Package goPasetoV4 provides an implementation of the Maker interface
+// using Paseto (Platform-Agnostic Security Tokens). It includes functionality
+// to create and verify tokens securely.
 package goPasetoV4
 
 import (
 	"aidanwoods.dev/go-paseto"
+	"crypto/rand"
 	"github.com/google/uuid"
+	"os"
 	"strings"
 	"time"
 )
 
-// PasetoMaker is a struct that implements Maker interface
+// PasetoMaker is a struct that implements the Maker interface. It provides
+// methods for creating and verifying Paseto tokens.
 type PasetoMaker struct {
-	symmetricKey paseto.V4SymmetricKey
-	implicit     []byte
+	symmetricKey paseto.V4SymmetricKey // symmetricKey is used for token encryption and decryption.
+	implicit     []byte                // implicit is an additional layer of security for token operations.
 }
 
+// NewPasetoMaker initializes and returns a new PasetoMaker. It attempts to
+// acquire a nonce from the PASETO_NONCE environment variable. If not available,
+// it generates a random nonce. This function returns a Maker interface.
 func NewPasetoMaker() Maker {
-	return &PasetoMaker{paseto.NewV4SymmetricKey(), []byte("my implicit nonce")}
+	var nonce []byte
+	if envNonce := os.Getenv("PASETO_NONCE"); envNonce != "" {
+		nonce = []byte(envNonce)
+	} else {
+		// Fallback to generate a random nonce
+		nonce = make([]byte, 24)
+		if _, err := rand.Read(nonce); err != nil {
+			return nil
+		}
+	}
+
+	return &PasetoMaker{paseto.NewV4SymmetricKey(), nonce}
 }
 
-// CreateToken creates a new token for a specific username and duration
+// CreateToken generates a new Paseto token for a given username and duration.
+// It returns an encrypted token string or an error if the token generation fails.
 func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
-	// create paseto token
-	token := paseto.NewToken()
-	// Create uuid for token id
-	tokenID, err := uuid.NewRandom()
+
+	token := paseto.NewToken() // Initializes a new Paseto token
+
+	tokenID, err := uuid.NewRandom() // Generates a unique identifier for the token
 	if err != nil {
-		return "", err
+		return "", err // Returns an error if the token generation fails
 	}
-	// add data to the token.
+	// Adding necessary data to the token
 	err = token.Set("id", tokenID.String())
 	if err != nil {
 		return "", err
@@ -35,17 +56,21 @@ func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (
 	if err != nil {
 		return "", err
 	}
+	// Setting issued at and expiration times for the token
 	token.SetIssuedAt(time.Now())
 	token.SetExpiration(time.Now().Add(duration))
+	// Encrypts and returns the token
 	return token.V4Encrypt(maker.symmetricKey, maker.implicit), nil
 }
 
-// VerifyToken checks if the token is valid or not
+// VerifyToken checks the validity of a provided Paseto token. It returns a Payload
+// struct if the token is valid or an error if it is invalid or expired.
 func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
 	parser := paseto.NewParser()
 	parser.AddRule(paseto.NotExpired())
 	parsedToken, err := parser.ParseV4Local(maker.symmetricKey, token, maker.implicit)
 	if err != nil {
+		// Handling different types of token parsing errors
 		if strings.Contains(err.Error(), "expired") {
 			return nil, ErrExpiredToken
 		}
@@ -60,6 +85,8 @@ func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
 
 }
 
+// getPayloadFromToken extracts and returns payload data from a parsed Paseto token.
+// It returns an error if the token data extraction fails.
 func getPayloadFromToken(t *paseto.Token) (*Payload, error) {
 	id, err := t.GetString("id")
 	if err != nil {
@@ -86,4 +113,5 @@ func getPayloadFromToken(t *paseto.Token) (*Payload, error) {
 	}, nil
 }
 
+// Ensuring PasetoMaker correctly implements the Maker interface
 var _ Maker = (*PasetoMaker)(nil)
--- paseto_maker_test.go
+++ paseto_maker_test.go
@@ -2,44 +2,54 @@ package goPasetoV4
 
 import (
 	"github.com/stretchr/testify/require"
-	"goPasetoV4/util"
 	"testing"
 	"time"
 )
 
-func TestPasetoMaker(t *testing.T) {
+func TestPasetoMakerTokenLifecycle(t *testing.T) {
+	// Testing token creation and validation lifecycle
 	maker := NewPasetoMaker()
+	require.NotNil(t, maker, "Maker should not be nil")
 
-	username := util.RandomOwner()
+	username := "testuser"
 	duration := time.Minute
 
-	issuedAt := time.Now()
-	expiredAt := issuedAt.Add(duration)
-
 	token, err := maker.CreateToken(username, duration)
-	require.NoError(t, err)
-	require.NotEmpty(t, token)
+	require.NoError(t, err, "Token creation should not error")
+	require.NotEmpty(t, token, "Token should not be empty")
 
 	payload, err := maker.VerifyToken(token)
-	require.NoError(t, err)
-	require.NotEmpty(t, payload)
+	require.NoError(t, err, "Token verification should not error")
+	require.NotEmpty(t, payload, "Payload should not be empty")
 
-	require.NotZero(t, payload.ID)
-	require.Equal(t, username, payload.Username)
-	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
-	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
-
+	require.NotZero(t, payload.ID, "Payload ID should not be zero")
+	require.Equal(t, username, payload.Username, "Username should match")
+	require.WithinDuration(t, time.Now(), payload.IssuedAt, time.Second, "IssuedAt should be recent")
+	require.WithinDuration(t, time.Now().Add(duration), payload.ExpiredAt, time.Second, "ExpiredAt should be correct")
 }
 
-func TestExpiredPasetoToken(t *testing.T) {
+func TestPasetoMakerExpiredToken(t *testing.T) {
+	// Testing behavior with an expired token
 	maker := NewPasetoMaker()
+	require.NotNil(t, maker, "Maker should not be nil")
 
-	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
-	require.NoError(t, err)
-	require.NotEmpty(t, token)
+	token, err := maker.CreateToken("expireduser", -time.Minute)
+	require.NoError(t, err, "Expired token creation should not error")
+	require.NotEmpty(t, token, "Expired token should not be empty")
 
 	payload, err := maker.VerifyToken(token)
-	require.Error(t, err)
-	require.EqualError(t, err, ErrExpiredToken.Error())
-	require.Nil(t, payload)
+	require.Error(t, err, "Expired token verification should error")
+	require.EqualError(t, err, ErrExpiredToken.Error(), "Error should be ErrExpiredToken")
+	require.Nil(t, payload, "Payload should be nil for an expired token")
+}
+
+func TestPasetoMakerInvalidToken(t *testing.T) {
+	// Testing behavior with an invalid token
+	maker := NewPasetoMaker()
+	require.NotNil(t, maker, "Maker should not be nil")
+
+	payload, err := maker.VerifyToken("invalidtoken")
+	require.Error(t, err, "Invalid token verification should error")
+	require.EqualError(t, err, ErrInvalidToken.Error(), "Error should be ErrInvalidToken")
+	require.Nil(t, payload, "Payload should be nil for an invalid token")
 }
